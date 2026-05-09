28afcce463acad8086de511c08e44c84caee509b
mdshantosharker <mdshantosharker@gmail.com>
2026-05-09 12:22:52 +0600

    added delete functionality

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -39,8 +39,16 @@ async function run() {
       };
       const user = await userCollection.findOne(query);
 
-      console.log("user id", id);
       res.send(user);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
